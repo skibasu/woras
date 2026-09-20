827f280deb52c15761be874fda7e095a06453b4e
suman <skibasu@gmail.com>
2026-09-20 12:53:09 +0200

    Init next config updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-    output: "export"
-};
+    output: "export",
+    experimental: {
+        cpus: 1,
+        workerThreads: false,
+    },
+}
 
-export default nextConfig;
+export default nextConfig
