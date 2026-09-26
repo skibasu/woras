ccadd7be5c471840ba803020a11d97c6f5aba656
suman <skibasu@gmail.com>
2026-09-26 02:12:47 +0200

    Working on cache

--- lib/wpgraphql.ts
+++ lib/wpgraphql.ts
@@ -1,3 +1,9 @@
 import { GraphQLClient } from "graphql-request"
 
-export const wordpressClient = new GraphQLClient("https://px661515.pxcloud.pl/cms/graphql")
+export const wordpressClient = new GraphQLClient("https://px661515.pxcloud.pl/cms/graphql", {
+    fetch: (url, options) =>
+        fetch(url, {
+            ...options,
+            cache: "no-store",
+        }),
+})
